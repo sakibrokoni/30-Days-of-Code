e6aae1034a9f3c2ac5320d7ca17e4ce23b12032a
Sakib Rokoni <sakib15-12961@diu.edu.bd>
2022-11-04 14:42:50 +0600

    DAY_16

---
+++ H - Exceptions - String to Integer/Solution.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+#  Write Your Code 
+try:
+    print(int(input().strip()))
+except ValueError:
+    print("Bad String")
